dde2001e1b8f7fc2e83f65811ca216d648c997f0
Abel Orihuela <abelorihuelamendoza@hotmail.com>
2021-09-10 12:18:10 -0500

    Updated new job

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,8 +21,12 @@ const IndexPage = () => (
         <div class="media-content">
           <p class="title is-4 has-text-white">Abel Orihuela</p>
           <p class="subtitle is-6 has-text-white">@abelhOrihuela</p>
+          
           <p class="subtitle is-6 has-text-white">
-            Software engineer. Experienced with all stages of the development cycle for web projects.
+            Software engineer with 7+ years of experience developing web applications for fields such as fintech, wellness, bioinformatic, ecommerce and data analysis. I have great problem-solving skills I am highly proficient. I am interested in the challenges of data analysis, data processing, functional programming and devops and I am interested in the usage of python and javascript.
+          </p>
+          <p class="subtitle is-6 has-text-white">
+            Experienced with all stages of the development cycle for web projects.
           </p>
         </div>
       </div>
@@ -37,15 +41,37 @@ const IndexPage = () => (
       <div className='card-content'>
         <div>
           <p>
-            <strong>Common Sense People | Team Lead - CTO | December 2017</strong>
+            <strong>Globant | Software Engineer | April 2021</strong>
           </p>
           <p>
-            Planning and develop digital products for multiple areas.
+            Contributed to building an IDE for industrial automation.
+          </p>
+        
+          <p>
+            Tech stack:
+            <i>
+              {
+                ` Kotlin - Angular - GraphQL - SQL`
+              }
+              
+                </i>
+          </p>
+        </div>
+        <br />
+        <div>
+          <p>
+            <strong>Common Sense People | Team Lead - CTO | December 2017 - April 2021</strong>
+          </p>
+          <p>
+            Planning and develop digital products for multiple areas such as fintech, wellness, bioinformatic, ecommerce and data analysis.
             </p>
           <p>
+            Tech stack:&nbsp;
             <i>
-              Eccomerce - Healthy - Bioinformatica - Movilidad - Security
-                </i>
+              {
+                ` Node JS - React JS - Next JS - Python - MongoDB - MySQL - PostgreSQL`
+              }
+            </i>
           </p>
         </div>
         <br />
@@ -54,12 +80,15 @@ const IndexPage = () => (
             <strong>Bit Technologies | Developer fullstack | July 2017 - December 2017</strong>
           </p>
           <p>
-            Develop tools for multiple areas
-              </p>
+            Contributed to building products for multiple areas such as elearning and wellnes.
+          </p>
           <p>
+            Tech stack:&nbsp;
             <i>
-              Checkout - Healthy - Education
-                </i>
+              {
+                ` Ruby on Rails - Angular - MySQL`
+              }
+            </i>
           </p>
         </div>
         <br />
@@ -68,12 +97,15 @@ const IndexPage = () => (
             <strong>Xpertys | Developer fullstack | May 2016 - July 2017</strong>
           </p>
           <p>
-            Develop core for market insurance and finnancial
-              </p>
+            Contributed to building a platform core for market insurance and finnancial.
+            </p>
           <p>
+            Tech stack:&nbsp;
             <i>
-              Insurance - Finnancial
-                </i>
+              {
+                ` Java - Angular - PLSQL - SQL`
+              }
+            </i>
           </p>
         </div>
       </div>
@@ -81,86 +113,33 @@ const IndexPage = () => (
     </div>
     <br />
 
-    <h2 className='title is-2 has-text-white'>
-      Technologies
-    </h2>
-
-    <div className='columns is-multiline is-mobile'>
-      <div className='column is-one-quarter-mobile'>
-        <figure class="image is-square image-shadow">
-          <img src={'java180x180.jpg'} alt="Logo" />
-        </figure>
-      </div>
-      <div className='column is-one-quarter-mobile'>
-        <figure class="image is-square image-shadow">
-          <img src={'Ruby-on-Rails.png'} alt="Logo" />
-        </figure>
-      </div>
-
-
-      <div className='column is-one-quarter-mobile'>
-        <figure class="image is-square image-shadow">
-          <img src={'laravel.jpg'} alt="Logo" />
-        </figure>
-      </div>
-
-      <div className='column is-one-quarter-mobile'>
-        <figure class="image is-square image-shadow">
-          <img src={'js180x180.png'} alt="Logo" />
-        </figure>
-      </div>
-      <div className='column is-one-quarter-mobile'>
-        <figure class="image is-square image-shadow">
-          <img src={'Koa.png'} alt="Logo" />
-        </figure>
-      </div>
-      <div className='column is-one-quarter-mobile'>
-        <figure class="image is-square image-shadow">
-          <img src={'react180x180.jpeg'} alt="Logo" />
-        </figure>
-      </div>
-      <div className='column is-one-quarter-mobile'>
-        <figure class="image is-square image-shadow">
-          <img src={'mongodb.jpg'} alt="Logo" />
-        </figure>
-      </div>
-      <div className='column is-one-quarter-mobile'>
-        <figure class="image is-square image-shadow">
-          <img src={'pgsql.jpg'} alt="Logo" />
-        </figure>
-      </div>
-    </div>
-
-
-    <hr></hr>
-
 
     <div className='columns'>
-      <div className='column'>
+      <div className='column is-2'>
         <figure class="image image image-shadow is-5by4">
           <img src={'platzi-tals.jpeg'} alt="Logo" />
         </figure>
         <br />
         <h3 className='title is-5 has-text-white'>
-          Talk about React Native | Platzi Developer Group CDMX
+          React Native Talk | Platzi Developer Group CDMX
         </h3>
       </div>
-      <div className='column'>
+      <div className='column is-2'>
         <figure class="image image image-shadow is-5by4">
           <img src={'atom-react.jpg'} alt="Logo" />
         </figure>
         <br />
         <h3 className='title is-5 has-text-white'>
-          Talk about React | Atom Academy Toluca
+          React Talk | Atom Academy Toluca
         </h3>
       </div>
-      <div className='column'>
+      <div className='column is-2'>
         <figure class="image image image-shadow is-5by4">
           <img src={'react-univer.jpg'} alt="Logo" />
         </figure>
         <br />
         <h3 className='title is-5 has-text-white'>
-          Talk about React Native + Expo | Univer Milenium Toluca
+          React Native + Expo Talk | Univer Milenium Toluca
         </h3>
       </div>
     </div>
